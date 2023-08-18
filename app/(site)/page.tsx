0e9ea6545a384bf3cf65409358e2b526e9fb0750
S. M. A Sayed Al Banna <2021591@iub.edu.bd>
2023-08-18 22:55:45 +0600

    trying to fix

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,15 +1,5 @@
-"use client"
-
-import supabase from '@/config/supabase'
-import Image from 'next/image'
-import { Database } from '@/types_db'
-import {useEffect, useState} from "react"
-import Main from '@/components/main'
-
 export default function Home() {
-
-
   return (
-    <Main/>
+    <div>Main Page</div>
   )
 }
--- components/Register.tsx
+++ components/Register.tsx
@@ -3,13 +3,12 @@ import { twMerge } from "tailwind-merge";
 import { BsArrowUpRight } from "react-icons/bs";
 import Link from "next/link";
 
-const Register = ({
-  className
-}) => {
+const Register = (
+) => {
   return (
     <Link
       href="registration"
-      className={twMerge(`border-1 gap-2 px-2 rounded text-black my-6 bg-button hidden md:flex items-center`,className)}
+      className={twMerge(`border-1 gap-2 px-2 rounded text-black my-6 bg-button hidden md:flex items-center`)}
     >
       <div className="text-xl font-bold">Register</div>
       <BsArrowUpRight fontSize={24} />
