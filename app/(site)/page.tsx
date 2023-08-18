a9a3fbf891bc5b18d423c51353e0e389fa575329
S. M. A Sayed Al Banna <2021591@iub.edu.bd>
2023-08-18 23:04:58 +0600

    fixed

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,14 @@
+"use client"
+
+import supabase from '@/config/supabase'
+import Image from 'next/image'
+import {useEffect, useState} from "react"
+import Main from '@/components/main'
+
 export default function Home() {
+
+
   return (
-    <div>Main Page</div>
+    <Main/>
   )
 }
--- components/Register.tsx
+++ components/Register.tsx
@@ -3,12 +3,13 @@ import { twMerge } from "tailwind-merge";
 import { BsArrowUpRight } from "react-icons/bs";
 import Link from "next/link";
 
-const Register = (
-) => {
+const Register = ({
+  className
+}) => {
   return (
     <Link
       href="registration"
-      className={twMerge(`border-1 gap-2 px-2 rounded text-black my-6 bg-button hidden md:flex items-center`)}
+      className={twMerge(`border-1 gap-2 px-2 rounded text-black my-6 bg-button hidden md:flex items-center`,className)}
     >
       <div className="text-xl font-bold">Register</div>
       <BsArrowUpRight fontSize={24} />
