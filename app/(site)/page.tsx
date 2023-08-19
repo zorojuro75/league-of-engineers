f39723163426abfec2fffe742ac1147bb44b47a4
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-08-18 22:43:17 -0700

    Home Page and RegiBut

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,15 @@
+"use client"
+
+import supabase from '@/config/supabase'
 import Image from 'next/image'
+import { Database } from '@/types_db'
+import {useEffect, useState} from "react"
+import Main from '@/components/main'
 
 export default function Home() {
+
+
   return (
-    <div>Main content</div>
+    <Main/>
   )
 }
