ea0313548218d0c4ef70066b14e29659f5428e76
S. M. A Sayed Al Banna <2021591@iub.edu.bd>
2023-09-11 22:52:24 +0600

    carousel completed

--- app/about/components/Overview.tsx
+++ app/about/components/Overview.tsx
@@ -1,8 +1,10 @@
 "use client";
 import React, {useEffect, useState} from "react";
 import Image from "next/image";
-import { Carousel } from "@material-tailwind/react";
+import { Carousel, IconButton } from "@material-tailwind/react";
+import type { CarouselProps } from "@material-tailwind/react";
 import supabase from "@/config/supabase";
+
 
 const Overview = () => {
   const [fetchError, setFetchError] = useState<any>(null)
@@ -50,13 +52,66 @@ const Overview = () => {
       </div>
       <div className="w-[50%]">
         {imgData && (
-          <Carousel>
+          <Carousel
+          className="rounded-xl h-[380px]"
+          prevArrow={({ handlePrev }) => (
+            <IconButton
+              variant="text"
+              color="orange"
+              size="lg"
+              onClick={handlePrev}
+              className="!absolute top-2/4 left-4 -translate-y-2/4"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                />
+              </svg>
+            </IconButton>
+          )}
+          nextArrow={({ handleNext }) => (
+            <IconButton
+              variant="text"
+              color="orange"
+              size="lg"
+              onClick={handleNext}
+              className="!absolute top-2/4 !right-4 -translate-y-2/4"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                />
+              </svg>
+            </IconButton>
+          )}
+        >
+    
             {imgData.map((data:any) => (
-              <img
+              <Image
                 src={data.image}
                 alt=""
                 key={data.id}
-                className="h-full w-full object-cover"
+                width={600}
+                height={400}
+                className="ms-[100px] border-2 border-black "
               />
             ))}
           </Carousel>
--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     experimental:{
         serverActions: true,
     },
+    images: {
+        domains: ['jslpvxgcjqptnldsnwvx.supabase.co'], // Add your domain(s) here
+      },
 }
 
 module.exports = nextConfig
