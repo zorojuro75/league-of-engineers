5b4d3fc7889d4cada752b71bab4a71d02d29fae0
S. M. A Sayed Al Banna <2021591@iub.edu.bd>
2023-09-25 11:19:40 +0600

    home page fixed

--- app/registration/components/RegButton.tsx
+++ app/registration/components/RegButton.tsx
@@ -22,7 +22,7 @@ const RegButton = (props: Props) => {
       </div>
       {isBlurred && (
   <div className="fixed inset-0 flex items-center justify-center backdrop-blur-sm bg-black bg-opacity-50">
-    <div className="bg-white p-6 rounded-lg shadow-lg overflow-y-auto md:overflow-y-hidden md:max-h-[100vh] max-h-[80vh]">
+    <div className="bg-white p-6 rounded-lg shadow-lg overflow-y-auto lg:overflow-y-hidden md:max-h-[100vh] max-h-[80vh]">
       <div onClick={callForm} className="relative md:right-[-770px] text-xl font-bold cursor-pointer border-2 border-black w-[25px] h-[25px] flex justify-center items-center text-red-400">x</div>
       <div>{props.children}</div>
     </div>
