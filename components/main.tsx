beccfb652dedb111ebc7794d5e12575024be12fc
S. M. A Sayed Al Banna <2021591@iub.edu.bd>
2023-10-03 09:35:54 +0600

    female players added

--- components/main.tsx
+++ components/main.tsx
@@ -7,14 +7,14 @@ export default function Main() {
     <div className="flex flex-col flex-1 grow-1">
       <div className="flex-1 grow-1 md:bg-[url('/kicking.png')] bg-no-repeat bg-contain bg-right-bottom mt-12">
         <div className="text-black md:w-[38.96%] mx-[20px]">
-          <div className="md:ms-[16%] ">
+          <div className="md:ms-[16%] flex flex-col gap-y-10">
             <h1 className="md:w-[566px] md:text-[60px] text-4xl font-bold">
               League of Engineers
             </h1>
             <p className="md:w-[150px] md:text-[32px] text-2xl font-semibold">
               Season <span className={`${styles.wrapper}`}>2</span>
             </p>
-            <p className="my-[22px] md:w-[403px] text-[#667085] text-[20px]">
+            <p className="my-[10px] md:w-[403px] text-[#667085] text-[20px]">
               Hey there, SETS students! The League of Engineers Season 2 is
               coming back and it&apos;s super exciting! Here&apos;s the deal: if
               you&apos;re a student from SETS, you can sign up all by yourself.
