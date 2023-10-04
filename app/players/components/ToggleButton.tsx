b5553a5ebac7858e8b2deadd3b3409a294aa619c
S. M. A Sayed Al Banna <2021591@iub.edu.bd>
2023-10-05 01:37:05 +0600

    player page

--- app/players/components/ToggleButton.tsx
+++ app/players/components/ToggleButton.tsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {
+  selectedTab: string;
+  setTabs: (newTab: string) => void;
+};
 
 const ToggleButton = (props: Props) => {
   return (
-    <div className='bg-green-500 bg-opacity-50 shadow-lg rounded-full h-[60px] w-[160px] flex'>
-        <div className='bg-green-500 shadow-lg rounded-full h-[60px] w-[60px]'></div>
-        <div className='flex justify-center items-center w-[100px] text-2xl font-bold'>Female</div>
+    <div className="bg-[#00212E] shadow-lg rounded-xl h-[60px] w-full flex items-center text-2xl text-white mb-10 cursor-pointer">
+      <div
+        onClick={() => props.setTabs("Male")}
+        className={`${
+          props.selectedTab === "Male" ? "bg-[#E3A02B]" : ""
+        } h-full w-1/3 flex items-center justify-center rounded-s-lg`}
+      >
+        Male
+      </div>
+      <div
+        onClick={() => props.setTabs("Female")}
+        className={`${
+          props.selectedTab === "Female" ? "bg-[#E3A02B]" : ""
+        } h-full w-1/3 flex items-center justify-center border-x-4 border-yellow-800`}
+      >
+        Female
+      </div>
+      <div
+        onClick={() => props.setTabs("Manager")}
+        className={`${
+          props.selectedTab === "Manager" ? "bg-[#E3A02B]" : ""
+        } h-full w-1/3 flex items-center justify-center rounded-e-lg`}
+      >
+        Manager
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ToggleButton
+export default ToggleButton;
--- app/players/page.tsx
+++ app/players/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import PlayerCard from "./components/PlayerCard";
 import supabase from "@/config/supabase";
+import ToggleButton from "./components/ToggleButton";
 
 type Player = {
   id: number;
@@ -23,6 +24,7 @@ const Page = () => {
   const [players, setPlayers] = useState<Player[]>([]);
   const [femalePlayers, setFemalePlayers] = useState<Player[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const [tabs, setTabs] = useState<string>("Female");
 
   useEffect(() => {
     async function fetchPlayers() {
@@ -31,8 +33,7 @@ const Page = () => {
           .from("formPlayer")
           .select()
           .eq("verified", true)
-          .eq("gender", "Male")
-          
+          .eq("gender", "Male");
 
         if (error) {
           setError(error.message);
@@ -50,8 +51,7 @@ const Page = () => {
           .from("formPlayer")
           .select()
           .eq("verified", true)
-          .eq("gender", "Female")
-          
+          .eq("gender", "Female");
 
         if (error) {
           setError(error.message);
@@ -66,40 +66,61 @@ const Page = () => {
 
     fetchPlayers();
     fetchFemalePlayers();
-  }, [players, setPlayers, femalePlayers, setFemalePlayers]); 
+  }, [players, setPlayers, femalePlayers, setFemalePlayers]);
 
   return (
     <>
       <div className="px-5">
         <section className="max-w-[90rem] mx-auto px-4 sm:px-6 lg:px-4 py-12">
-          <div className="text-center pb-12">
-            <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
-              Meet Our Players
-            </h1>
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-x-[8rem] gap-y-6">
-            {players
-              ? players.map((player) => (
-                  <div key={player.id} className="w-full rounded-lg px-14 py-16 flex flex-col justify-center items-center bg-white shadow-2xl">
-                    <PlayerCard  player={player} />
-                  </div>
-                ))
-              : null}
-          </div>
-          <div className="text-center mt-10 p-12">
-            <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
-              Meet Our Female Players
-            </h1>
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-x-[8rem] gap-y-6">
-            {femalePlayers
-              ? femalePlayers.map((player) => (
-                  <div key={player.id} className="w-full rounded-lg px-14 py-16 flex flex-col justify-center items-center bg-white shadow-2xl">
-                    <PlayerCard  player={player} />
-                  </div>
-                ))
-              : null}
-          </div>
+          <ToggleButton setTabs={setTabs} selectedTab={tabs}/>
+          {tabs == "Male" ? (
+            <>
+              <div className="text-center pb-12">
+                <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
+                  Meet Our Players
+                </h1>
+              </div>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-x-[8rem] gap-y-6">
+                {players
+                  ? players.map((player) => (
+                      <div
+                        key={player.id}
+                        className="w-full rounded-lg px-14 py-16 flex flex-col justify-center items-center bg-white shadow-2xl"
+                      >
+                        <PlayerCard player={player} />
+                      </div>
+                    ))
+                  : null}
+              </div>
+            </>
+          ) : tabs == "Female" ? (
+            <>
+              <div className="text-center pb-12">
+                <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
+                  Meet Our Female Players
+                </h1>
+              </div>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-x-[8rem] gap-y-6">
+                {femalePlayers
+                  ? femalePlayers.map((player) => (
+                      <div
+                        key={player.id}
+                        className="w-full rounded-lg px-14 py-16 flex flex-col justify-center items-center bg-white shadow-2xl"
+                      >
+                        <PlayerCard player={player} />
+                      </div>
+                    ))
+                  : null}
+              </div>
+            </>
+          ) : (
+            <>
+            <div className="text-center pb-12">
+                <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
+                  Meet Our Managers
+                </h1>
+              </div></>
+          )}
         </section>
       </div>
     </>
