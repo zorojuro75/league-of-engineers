b102fb8ad5bb8ffa40b43fc1ef8ba09844e129eb
S. M. A Sayed Al Banna <2021591@iub.edu.bd>
2023-10-09 23:36:52 +0600

    registration is over

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -13,12 +13,16 @@ const Registration = () => {
 
   return (
     <>
-      <RegistrationProcess />
+      {/* <RegistrationProcess />
       <Payment />
       <div className="text-2xl font-semibold md:mx-auto">Player Registration fee 200tk</div>
       <RegButton>
         <Form />
-      </RegButton>
+      </RegButton> */}
+      <div className="max-w-7xl md:mx-auto flex flex-col gap-y-5 justify-center items-center text-center text-3xl font-bold text-blue-gray-800 flex-1 mx-5">
+        <div>Registration for League of Engineers is now closed. </div>
+        <div>Thank you for your overwhelming response, and we look forward to an exciting tournament!</div>
+      </div>
     </>
   );
 };
