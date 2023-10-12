d4ff7689135b34efee705c8eadfc6a0f585b2422
S. M. A Sayed Al Banna <2021591@iub.edu.bd>
2023-10-13 01:29:16 +0600

    teams updated

---
+++ app/teams/components/Data.tsx
@@ -0,0 +1,39 @@
+'use client'
+import React, { useEffect, useState } from "react";
+import TeamDetails from "./TeamDetails";
+import supabase from "@/config/supabase";
+type Props = {};
+
+const Data = (props: Props) => {
+  const [teams, setTeams] = useState<string[] | undefined>(undefined);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchTeam() {
+      try {
+        const { data, error } = await supabase.from("Team").select("teamName");
+        if (error) {
+          setError(error.message);
+        } else {
+          const sortedTeams = data
+          ?.map((team) => team.teamName)
+          .sort((a, b) => a.localeCompare(b));
+        setTeams(sortedTeams);
+        }
+      } catch (e) {
+        setError("An unexpected error occurred while fetching team names.");
+        console.error("An unexpected error occurred:", e);
+      }
+    }
+    fetchTeam();
+  }, []);
+  return (
+    <div className="md:max-w-7xl w-full md:mx-auto flex flex-col my-10 gap-5">
+      {teams
+        ? teams.map((team) => <TeamDetails key={team} teamName={team} />)
+        : null}
+    </div>
+  );
+};
+
+export default Data;
--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -1,38 +1,11 @@
-'use client'
-import React, { useEffect, useState } from "react";
-import TeamDetails from "./components/TeamDetails";
-import supabase from "@/config/supabase";
+import React from "react";
+import Data from "./components/Data";
 type Props = {};
 
 const page = (props: Props) => {
-  const [teams, setTeams] = useState<string[] | undefined>(undefined);
-  const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    async function fetchTeam() {
-      try {
-        const { data, error } = await supabase.from("Team").select("teamName");
-        if (error) {
-          setError(error.message);
-        } else {
-          const sortedTeams = data
-          ?.map((team) => team.teamName)
-          .sort((a, b) => a.localeCompare(b));
-        setTeams(sortedTeams);
-        }
-      } catch (e) {
-        setError("An unexpected error occurred while fetching team names.");
-        console.error("An unexpected error occurred:", e);
-      }
-    }
-    fetchTeam();
-  }, []);
   return (
-    <div className="md:max-w-7xl w-full md:mx-auto flex flex-col my-10 gap-5">
-      {teams
-        ? teams.map((team) => <TeamDetails key={team} teamName={team} />)
-        : null}
-    </div>
+    <Data/>
   );
 };
 
