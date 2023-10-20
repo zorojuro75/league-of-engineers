eefc2b57a6bc011b68682cea492dafb9908827e6
S. M. A Sayed Al Banna <2021591@iub.edu.bd>
2023-10-21 01:43:39 +0600

    points table

--- app/fixtures/page.tsx
+++ app/fixtures/page.tsx
@@ -39,7 +39,7 @@ const Page = (props: Props) => {
               return a.played ? 1 : -1;
             }
           });
-    
+
           setFixtures(data || []);
         }
       } catch (e) {
@@ -97,88 +97,10 @@ const Page = (props: Props) => {
   });
   return (
     <>
-    {fixturesSF.length!=0?
-    <div className="w-max-7xl shadow-xl bg-white mx-auto grid md:grid-cols-2 grid-cols-1 place-content-between gap-x-20 gap-y-5 p-10 rounded-lg my-10 text-sm md:text-lg">
-      <div className="md:col-span-2 text-center text-3xl font-bold">Semi Final</div>
-    {fixturesSF.map((match) => (
-              <div
-                key={match.matchID}
-                className={`md:w-[450px] flex items-center px-2 md:px-10 py-2 ${
-                  match.groupName === "A"
-                    ? "bg-green-500 bg-opacity-20"
-                    : match.groupName === "B"
-                    ? "bg-blue-500 bg-opacity-20"
-                    : match.groupName === "C"
-                    ? "bg-yellow-500 bg-opacity-20"
-                    : match.groupName === "D"
-                    ? "bg-red-500 bg-opacity-20"
-                    : "bg-purple-500 bg-opacity-20"
-                }`}
-              >
-                <div className="flex justify-between md:w-[65%] w-[60%] px-2 bg-group-color">
-                  <div>
-                    <div>{match.homeTeam}</div>
-                    <div>{match.awayTeam}</div>
-                  </div>
-                  <div>
-                    <div>{match.homeGoal}</div>
-                    <div>{match.awayGoal}</div>
-                  </div>
-                </div>
-                {
-                match.played!=true?<div className="border-s border-black border-opacity-40 md:ps-5 ps-1">
-                <div>{match.matchDate}</div>
-                <div>{match.matchTime}</div>
-                <div>{match.matchDay}</div>
-              </div>:
-              <div className="border-s border-black border-opacity-40 md:ps-5 ps-1">FT</div>
-                }
-                
-              </div>
-            ))
-              }
-          
-      </div>: null}
-    {fixturesQF.length!=0?
-    <div className="w-max-7xl shadow-xl bg-white mx-auto grid md:grid-cols-2 grid-cols-1 place-content-between gap-x-20 gap-y-5 p-10 rounded-lg my-10 text-sm md:text-lg">
-      <div className="md:col-span-2 text-center text-3xl font-bold">Quarter Final</div>
-    {fixturesQF.map((match) => (
-              <div
-                key={match.matchID}
-                className={`md:w-[450px] flex items-center px-2 md:px-10 py-2 ${
-                  match.groupName === "A"
-                    ? "bg-green-500 bg-opacity-20"
-                    : match.groupName === "B"
-                    ? "bg-blue-500 bg-opacity-20"
-                    : match.groupName === "C"
-                    ? "bg-yellow-500 bg-opacity-20"
-                    : match.groupName === "D"
-                    ? "bg-red-500 bg-opacity-20"
-                    : "bg-purple-500 bg-opacity-20"
-                }`}
-              >
-                <div className="flex justify-between md:w-[65%] w-[60%] px-2 bg-group-color">
-                  <div>
-                    <div>{match.homeTeam}</div>
-                    <div>{match.awayTeam}</div>
-                  </div>
-                  <div>
-                    <div>{match.homeGoal}</div>
-                    <div>{match.awayGoal}</div>
-                  </div>
-                </div>
-                <div className="border-s border-black border-opacity-40 md:ps-5 ps-1">
-                  <div>{match.matchDate}</div>
-                  <div>{match.matchTime}</div>
-                  <div>{match.matchDay}</div>
-                </div>
-              </div>
-            ))
-              }
-          
-      </div>: null}
       <div className="w-max-7xl shadow-xl bg-white mx-auto grid md:grid-cols-2 grid-cols-1 place-content-between gap-x-20 gap-y-5 p-10 rounded-lg my-10 text-sm md:text-lg">
-        <div className="md:col-span-2 text-center text-3xl font-bold">Group Stage</div>
+        <div className="md:col-span-2 text-center text-3xl font-bold">
+          Group Stage
+        </div>
         {fixtures
           ? fixtures.map((match) => (
               <div
@@ -205,18 +127,105 @@ const Page = (props: Props) => {
                     <div>{match.awayGoal}</div>
                   </div>
                 </div>
-                {
-                match.played!=true?<div className="border-s border-black border-opacity-40 md:ps-5 ps-1">
-                <div>{match.matchDate}</div>
-                <div>{match.matchTime}</div>
-                <div>{match.matchDay}</div>
-              </div>:
-              <div className="border-s border-black border-opacity-40 md:ps-5 ps-1">Full Time</div>
-                }
+                {match.played != true ? (
+                  <div className="border-s border-black border-opacity-40 md:ps-5 ps-1">
+                    <div>{match.matchDate}</div>
+                    <div>{match.matchTime}</div>
+                    <div>{match.matchDay}</div>
+                  </div>
+                ) : (
+                  <div className="border-s border-black border-opacity-40 md:ps-5 ps-1">
+                    Full Time
+                  </div>
+                )}
               </div>
             ))
           : null}
       </div>
+      {fixturesQF.length != 0 ? (
+        <div className="w-max-7xl shadow-xl bg-white mx-auto grid md:grid-cols-2 grid-cols-1 place-content-between gap-x-20 gap-y-5 p-10 rounded-lg my-10 text-sm md:text-lg">
+          <div className="md:col-span-2 text-center text-3xl font-bold">
+            Quarter Final
+          </div>
+          {fixturesQF.map((match) => (
+            <div
+              key={match.matchID}
+              className={`md:w-[450px] flex items-center px-2 md:px-10 py-2 ${
+                match.groupName === "A"
+                  ? "bg-green-500 bg-opacity-20"
+                  : match.groupName === "B"
+                  ? "bg-blue-500 bg-opacity-20"
+                  : match.groupName === "C"
+                  ? "bg-yellow-500 bg-opacity-20"
+                  : match.groupName === "D"
+                  ? "bg-red-500 bg-opacity-20"
+                  : "bg-purple-500 bg-opacity-20"
+              }`}
+            >
+              <div className="flex justify-between md:w-[65%] w-[60%] px-2 bg-group-color">
+                <div>
+                  <div>{match.homeTeam}</div>
+                  <div>{match.awayTeam}</div>
+                </div>
+                <div>
+                  <div>{match.homeGoal}</div>
+                  <div>{match.awayGoal}</div>
+                </div>
+              </div>
+              <div className="border-s border-black border-opacity-40 md:ps-5 ps-1">
+                <div>{match.matchDate}</div>
+                <div>{match.matchTime}</div>
+                <div>{match.matchDay}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : null}
+      {fixturesSF.length != 0 ? (
+        <div className="w-max-7xl shadow-xl bg-white mx-auto grid md:grid-cols-2 grid-cols-1 place-content-between gap-x-20 gap-y-5 p-10 rounded-lg my-10 text-sm md:text-lg">
+          <div className="md:col-span-2 text-center text-3xl font-bold">
+            Semi Final
+          </div>
+          {fixturesSF.map((match) => (
+            <div
+              key={match.matchID}
+              className={`md:w-[450px] flex items-center px-2 md:px-10 py-2 ${
+                match.groupName === "A"
+                  ? "bg-green-500 bg-opacity-20"
+                  : match.groupName === "B"
+                  ? "bg-blue-500 bg-opacity-20"
+                  : match.groupName === "C"
+                  ? "bg-yellow-500 bg-opacity-20"
+                  : match.groupName === "D"
+                  ? "bg-red-500 bg-opacity-20"
+                  : "bg-purple-500 bg-opacity-20"
+              }`}
+            >
+              <div className="flex justify-between md:w-[65%] w-[60%] px-2 bg-group-color">
+                <div>
+                  <div>{match.homeTeam}</div>
+                  <div>{match.awayTeam}</div>
+                </div>
+                <div>
+                  <div>{match.homeGoal}</div>
+                  <div>{match.awayGoal}</div>
+                </div>
+              </div>
+              {match.played != true ? (
+                <div className="border-s border-black border-opacity-40 md:ps-5 ps-1">
+                  <div>{match.matchDate}</div>
+                  <div>{match.matchTime}</div>
+                  <div>{match.matchDay}</div>
+                </div>
+              ) : (
+                <div className="border-s border-black border-opacity-40 md:ps-5 ps-1">
+                  FT
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      ) : null}
     </>
   );
 };
